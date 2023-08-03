9616946a4dca434a9af5e41ea3fbc5e5be75b3fc
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-03 11:44:01 +0100

    add: weather application code :construction:

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+/**
+ * Collect the place you need the weather forecast
+ * from the command line.
+ */
+const place = process.argv[2]
 
+if (place) {
+    geocode(place, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+    
+        forecast( data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+    
+} else {
+    console.log('Please provide the name of the place')
+}
 
+    
+// } else {
+//     console.log('Please provide the name of the place')
+// }
 // const url = 'http://api.weatherstack.com/current?access_token=<YOUR ACCESS TOKEN>&query=37.8267,-122.4233'
 
 // request({url: url, json: true}, (error, response) => {
@@ -16,20 +44,20 @@ const request = require('request')
 //     }
 // })
 
-const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=<YOUR ACCESS TOKEN>'
+// const mapurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=<YOUR ACCESS TOKEN>'
 
-request({ url: mapurl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather app')
-    } else if (response.body.features.length === 0) {
-        console.log('Can\'t find location')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(response.body.features[0].place_name)
-        console.log('latitude = ' + latitude, 'longitude = ' + longitude)
-    }
-})
+// request({ url: mapurl, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather app')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Can\'t find location')
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(response.body.features[0].place_name)
+//         console.log('latitude = ' + latitude, 'longitude = ' + longitude)
+//     }
+// })
 
 
 // request({url: url, json: true}, (error, response) => {
